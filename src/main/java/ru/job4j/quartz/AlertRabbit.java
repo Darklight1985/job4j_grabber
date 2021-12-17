d5c887fe4cc79ad4fbfdaed745819ca0e73a6664
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-17 22:17:32 +0300

    1. Quartz [#175122] исправил

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,7 +4,6 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
