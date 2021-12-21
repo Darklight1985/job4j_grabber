989dbf26d19de7c7ad45afcf5b73a71df0eb7205
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-21 19:48:56 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,6 +6,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
+/**
+ * В данном классе с помощью библиотеки jsoup парсируется первые пять страниц сайта
+ * Находим ссылку на вакансию и дату публикования вакансаии
+ */
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= 5; i++) {
@@ -14,9 +20,24 @@ public class SqlRuParse {
             for (Element td : row) {
                 Element href = td.child(0);
                 System.out.println(href.attr("href"));
+                postParse(href.attr("href"));
                 System.out.println(href.text());
                 System.out.println(td.parent().child(5).text());
             }
         }
+    }
+
+    /**
+     * Метод парисрует по ссылке описание вакансии и дату ее опубликования
+     * @param postStr
+     * @throws IOException
+     */
+    public static void postParse(String postStr) throws IOException {
+        Document document = Jsoup.connect(postStr).get();
+        Elements row = document.select(".msgBody");
+        Element td = row.get(1);
+            System.out.println(td.text());
+            Element data = td.parents().select(".msgFooter").first();
+        System.out.println(data.text().substring(0, data.text().indexOf("[")));
     }
 }
