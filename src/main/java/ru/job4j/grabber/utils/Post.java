6722353228cc63c29d86f74ca90a7a18c9a037c2
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-21 09:06:25 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -0,0 +1,50 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Модель данных
+ */
+public class Post {
+    /**
+     * поле id получаемое из БД
+     */
+    private int id;
+    /**
+     * Название вакансии
+     */
+    private String title;
+    /**
+     * Ссылка на описание вакансии
+     */
+    private String link;
+    /**
+     * Опсание вакансии
+     */
+    private String description;
+    /**
+     * Дата создания ваканссии
+     */
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(title, post.title)
+                && Objects.equals(link, post.link)
+                && Objects.equals(description, post.description)
+                && Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, link, description, created);
+    }
+}
