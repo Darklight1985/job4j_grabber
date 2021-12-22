0caf75557c27ec48ca0a4d1d223a318c64c7fcb0
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-23 00:01:22 +0300

    2.4. SqlRuParse [#285213 #242656]

--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -1,5 +1,7 @@
 package ru.job4j.grabber.utils;
 
+import org.jsoup.nodes.Element;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -27,6 +29,53 @@ public class Post {
      * Дата создания ваканссии
      */
     private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,8 +7,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.utils.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,26 +26,13 @@ public class SqlRuParse implements Parse {
             for (Element td : row) {
                 Element href = td.child(0);
                 System.out.println(href.attr("href"));
-                postParse(href.attr("href"));
                 System.out.println(href.text());
                 System.out.println(td.parent().child(5).text());
+                SqlRuParse sqlRuParse = new SqlRuParse();
+               Post post = sqlRuParse.detail(href.attr("href"));
+                System.out.println(post);
             }
         }
-    }
-
-    /**
-     * Метод парисрует по ссылке описание вакансии и дату ее опубликования
-     *
-     * @param postStr
-     * @throws IOException
-     */
-    public static void postParse(String postStr) throws IOException {
-        Document document = Jsoup.connect(postStr).get();
-        Elements row = document.select(".msgBody");
-        Element td = row.get(1);
-        System.out.println(td.text());
-        Element data = td.parents().select(".msgFooter").first();
-        System.out.println(data.text().substring(0, data.text().indexOf("[")));
     }
 
     @Override
@@ -65,7 +54,7 @@ public class SqlRuParse implements Parse {
         /**
          * Ссылка на вакансию
          */
-          String ssilka = null;
+        String ssilka = null;
         /**
          * Дата обновления поста
          */
@@ -81,23 +70,16 @@ public class SqlRuParse implements Parse {
         /**
          * Дата создания поста
          */
-          String crDate;
+          LocalDateTime crDate;
 
-        Document doc = Jsoup.connect(link).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            ssilka = href.attr("href");
-            publication = href.text();
-            postParse(href.attr("href"));
-            updDate = td.parent().child(5).text();
-        }
-        Document document = Jsoup.connect(ssilka).get();
+        Document document = Jsoup.connect(link).get();
+        publication = document.select(".messageHeader").get(0).ownText();
         Elements rowPost = document.select(".msgBody");
         Element td = rowPost.get(1);
         decription = rowPost.get(1).text();
         Element data = td.parents().select(".msgFooter").first();
-        crDate = data.text().substring(0, data.text().indexOf("["));
-        return new Post();
+        SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
+        crDate = timeParser.parse(data.text().substring(0, data.text().indexOf("[")));
+        return new Post(publication, link, decription, crDate);
     }
 }
