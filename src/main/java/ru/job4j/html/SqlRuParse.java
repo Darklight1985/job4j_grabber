525d91c8767c3b4de035e24043e91a9d708c5876
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-23 18:23:05 +0300

    2.4. SqlRuParse [#285213 #242656] исправил

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.Post;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
@@ -19,6 +20,12 @@ import java.util.List;
  * Находим ссылку на вакансию и дату публикования вакансаии
  */
 public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     public static void main(String[] args) throws Exception {
         for (int i = 1; i <= 5; i++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -28,20 +35,24 @@ public class SqlRuParse implements Parse {
                 System.out.println(href.attr("href"));
                 System.out.println(href.text());
                 System.out.println(td.parent().child(5).text());
-                SqlRuParse sqlRuParse = new SqlRuParse();
-               Post post = sqlRuParse.detail(href.attr("href"));
-                System.out.println(post);
             }
         }
     }
 
     @Override
     public List<Post> list(String link) {
-        SqlRuParse sqlRuParse = new SqlRuParse();
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
         List<Post> list = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             try {
-                list.add(sqlRuParse.detail(link + "/" + i));
+                Document doc = Jsoup.connect(link + "/" + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Post post = sqlRuParse.detail(td.child(0).attr("href"));
+                    if (!post.getTitle().equalsIgnoreCase("javascript")) {
+                        list.add(post);
+                    }
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -78,8 +89,8 @@ public class SqlRuParse implements Parse {
         Element td = rowPost.get(1);
         decription = rowPost.get(1).text();
         Element data = td.parents().select(".msgFooter").first();
-        SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
-        crDate = timeParser.parse(data.text().substring(0, data.text().indexOf("[")));
+        crDate = this.dateTimeParser.parse(data.text().substring(0,
+                data.text().indexOf("[")).trim());
         return new Post(publication, link, decription, crDate);
     }
 
