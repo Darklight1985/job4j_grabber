4327180006ffca04964826d58b6c8e3f23c6bcbd
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-27 23:08:49 +0300

    2. SQL Select by company [#2760 #244488] исправил 3

--- 2. SQL Select by company [#2760] 2.sql
+++ 2. SQL Select by company [#2760] 2.sql
@@ -3,9 +3,13 @@ join company c on
 p.company_id = c.id
 where c.id <> 5;
 
-select c.name, count(p.id) 
+select c.name, count(p.id) from person as p
+join company c on p.company_id = c.id
+group by c.name
+having count(p.id) =
+(select count(p.id) 
 from person p
 join company c on c.id = p.company_id
 group by c.name
 order by count(p.id) desc
-limit 1
+limit 1)
