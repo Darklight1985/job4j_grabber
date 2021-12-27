70c3702dc47aa21da5a407e5c171c63752680d47
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-12-27 22:53:52 +0300

    2. SQL Select by company [#2760 #244488] исправил2

---
+++ 2. SQL Select by company [#2760] 2.sql
@@ -0,0 +1,11 @@
+select p.name as сотрудник, c.name as Компания from person as p
+join company c on
+p.company_id = c.id
+where c.id <> 5;
+
+select c.name, count(p.id) 
+from person p
+join company c on c.id = p.company_id
+group by c.name
+order by count(p.id) desc
+limit 1
